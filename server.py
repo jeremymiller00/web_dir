0dd0b26795385b6e63d9adbc7307ccd710e35d65
Jeremy <jeremymiller00@gmail.com>
2018-10-31 09:44:09 -0700

    develop server

--- server.py
+++ server.py
@@ -4,10 +4,10 @@ import pickle
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
-@app.route('/index')
+# @app.route('/index')
 
 def home():
-    return '<p> Hello you </p>'
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(app.run(host ='0.0.0.0', port = 3333, debug = True))
